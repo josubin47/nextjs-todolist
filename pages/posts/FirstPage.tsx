d1f24b2396fa81714e8ad58f3f115da94a5c418d
josubin47 <3_3_jimca@naver.com>
2021-08-13 12:36:22 +0900

    feat : css

--- pages/posts/FirstPage.tsx
+++ pages/posts/FirstPage.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "components/Layout/Layout";
 
-export default function FirstPost() {
+export default function FirstPage() {
   return (
-    <>
+    <Layout home>
       <Head>
         <title>First Post</title>
       </Head>
+      <h1>First Post</h1>
       <Link href="/">
         <a>Back to home</a>
       </Link>
-    </>
+    </Layout>
   );
 }
